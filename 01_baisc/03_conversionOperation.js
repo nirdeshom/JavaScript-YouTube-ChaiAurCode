e3c5bc30a4414f3c5aaccfe2a8559974352adc1b
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-09-05 15:55:21 +0530

    convertion operation

---
+++ 01_baisc/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "33abc";
+
+// console.log(typeof (score));
+
+// convert string to number 
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1 / false => 0
+*/
+
+let isLoggedIn = 1;
+
+// console.log(typeof isLoggedIn);
+
+// convert number to boolean 
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/* 
+1 => true / 0 => false
+"" => false / "nir" => true
+*/
+
+let myScore = 33;
+
+// console.log( typeof myScore);
+
+let convertString = String(myScore);
+
+console.log(typeof convertString);
+console.log(convertString);
