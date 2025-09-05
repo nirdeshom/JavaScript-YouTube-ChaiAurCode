c4d06d198007adcf4d9ca7d07be2c3b28a4f5a70
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-09-05 15:15:51 +0530

    Basic DataType

---
+++ 01_baisc/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3 + 3);  // we are using nodejs, not browser
+
+console.log(3 + 
+    3
+);  // code readability should be high
+
+console.log("Nirdesh");
+
+let name = "Nirdesh Verma";
+let age = 23;
+let isLoggedIn = true;
+let city;
+
+// Number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined
+// Symbol => unique
+
+console.log(typeof name);
+console.log(typeof (name));
+console.log(typeof null);  // Object
+console.log(typeof undefined);  // undefined
